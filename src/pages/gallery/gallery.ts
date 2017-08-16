fa77736678020d0f6c62af42f12b2c11bd0c3803
Gustavo Youngberg <gyoungbe@gmail.com>
2017-08-16 16:06:17 -0700

    Update Gallery images
    Update gallery images and removed unused images.

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -1,12 +1,6 @@
-import { Component }      from '@angular/core';
-import { NavController }  from 'ionic-angular';
+import { Component, ViewChild }      from '@angular/core';
+import { NavController, Slides, LoadingController }  from 'ionic-angular';
 
-//import { jQuery }         from 'jquery/dist/jquery.min.js';
-import * as $ from "jquery";
-import "lightgallery";
-// import { lgFullscreen }   from 'lg-fullscreen/dist/lg-fullscreen.min.js';
-// import { lgThumbnail }    from 'lg-thumbnail/dist/lg-thumbnail.min.js';
-// import { lgZoom }         from 'lg-zoom/dist/lg-zoom.min.js';
 
 @Component({
   selector: 'page-gallery',
@@ -15,108 +9,60 @@ import "lightgallery";
 export class GalleryPage {
   // galleryImages: any[];
   selectedGallery: string;
+  public slider: any;
+  @ViewChild(Slides) slides: Slides;
 
-  galleryExterior: any[];
-  galleryInterior: any[];
-  galleryRendering: any[];
-
-  constructor(public navCtrl: NavController) {
-    this.galleryExterior = [
-        {
-          image: 'assets/img/exterior/exterior-01.jpg'
-        },
-        {
-          image: 'assets/img/exterior/exterior-02.jpg'
-        },
-        {
-          image: 'assets/img/exterior/exterior-03.jpg'
-        },
-        {
-          image: 'assets/img/exterior/exterior-04.jpg'
-        },
-        {
-          image: 'assets/img/exterior/exterior-05.jpg'
-        },
-        {
-          image: 'assets/img/exterior/exterior-06.jpg'
-        },
-        {
-          image: 'assets/img/exterior/exterior-07.jpg'
-        },
-        {
-          image: 'assets/img/exterior/exterior-08.jpg'
-        }
-    ]
-    this.galleryInterior = [
-        {
-          image: 'assets/img/interior/interior-01.jpg'
-        },
-        {
-          image: 'assets/img/interior/interior-02.jpg'
-        },
-        {
-          image: 'assets/img/interior/interior-03.jpg'
-        },
-        {
-          image: 'assets/img/interior/interior-04.jpg'
-        },
-        {
-          image: 'assets/img/interior/interior-05.jpg'
-        },
-        {
-          image: 'assets/img/interior/interior-06.jpg'
-        },
-        {
-          image: 'assets/img/interior/interior-07.jpg'
-        },
-        {
-          image: 'assets/img/interior/interior-08.jpg'
-        }
-    ]
-    this.galleryRendering = [
-        {
-          image: 'assets/img/rendering/rendering-01.jpg'
-        },
-        {
-          image: 'assets/img/rendering/rendering-02.jpg'
-        },
-        {
-          image: 'assets/img/rendering/rendering-03.jpg'
-        },
-        {
-          image: 'assets/img/rendering/rendering-04.jpg'
-        },
-        {
-          image: 'assets/img/rendering/rendering-05.jpg'
-        },
-        {
-          image: 'assets/img/rendering/rendering-06.jpg'
-        },
-        {
-          image: 'assets/img/rendering/rendering-07.jpg'
-        },
-        {
-          image: 'assets/img/rendering/rendering-08.jpg'
-        }
-    ]
-
+  constructor(public navCtrl: NavController, public loading: LoadingController) {
     this.selectedGallery = 'exterior';
-
 
   }
 
+  // Go To Slide Index
+  // slideToZero() {
+  //   this.slides.slideTo(0, 400);
+  // }
+  // slideToOne() {
+  //   this.slides.slideTo(1, 400);
+  // }
+  // slideToTwo() {
+  //   this.slides.slideTo(2, 400);
+  // }
+  // slideToThree() {
+  //   this.slides.slideTo(3, 400);
+  // }
+  // slideToFour() {
+  //   this.slides.slideTo(4, 400);
+  // }
+  // slideToFive() {
+  //   this.slides.slideTo(5, 400);
+  // }
+  // slideToSix() {
+  //   this.slides.slideTo(6, 400);
+  // }
+  // slideToSeven() {
+  //   this.slides.slideTo(7, 400);
+  // }
 
+
+  slideChanged() {
+    // let currentIndex = this.slides.getActiveIndex();
+    // console.log('Current index is', currentIndex);
+  }
+
+  presentLoading(){
+    let loader = this.loading.create({
+      content: 'Loading Gallery...'
+    });
+
+    loader.present();
+
+    setTimeout(() => {
+      loader.dismiss();
+    }, 1200);
+  }
 
   ionViewDidLoad() {
-
-    $(".lightGallery-1").lightGallery({
-      thumbnail:true,
-      selector: 'img'
-    });
-    $(".lightGallery-2").lightGallery({
-      thumbnail: true,
-      selector: 'img'
-    });
+    this.presentLoading();
   }
 
 }
