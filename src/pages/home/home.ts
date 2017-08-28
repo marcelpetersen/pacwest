48ee7825bcff662b48105ae71720c2870198a95c
Gustavo Youngberg <gyoungbe@gmail.com>
2017-08-28 16:39:51 -0700

    Fix Availabilities Bug and Updates
    Fix bug on iOS that pushes bars on Availabilities page up/down and
    keeps them from lining up with illustration.
    
    Add StatusBar to change to black text on Home Screen — This is not
    working at the moment.
    
    Update Config

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { StatusBar } from 'ionic-native';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -11,5 +11,16 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewWillEnter() {
+    StatusBar.styleBlackTranslucent();
+    console.log(StatusBar);
+  }
+
+  ionViewDidLeave() {
+    StatusBar.styleDefault();
+    console.log(StatusBar);
+  }
+
 
 }
